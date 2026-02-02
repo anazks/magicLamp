e93c33b915c3b3d3fe22019ea03188882b174db4
anazks <anazksunil2@gmail.com>
2026-02-02 14:06:21 +0530

    added new chnages in admin

--- src/Admin/Components/Stats.tsx
+++ src/Admin/Components/Stats.tsx
@@ -217,7 +217,7 @@ export default function Stats() {
                 cx="50%"
                 cy="50%"
                 labelLine={false}
-                label={({ role, percent }) => `${role} ${(percent * 100).toFixed(0)}%`}
+                label={({ role, percent }) => `${role} ${((percent ?? 0) * 100).toFixed(0)}%`}
                 outerRadius={80}
                 fill="#8884d8"
                 dataKey="count"
